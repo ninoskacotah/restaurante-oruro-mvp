c6fddf925d529528b220ae8f34a75dd966f103be
ninoskacotah <ninoskacota@gmail.com>
2026-07-28 22:26:46 -0400

    feat: implementar modelo persistente de plato

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,8 +2,15 @@
 
 from app.models.administrador import Administrador
 from app.models.cliente import Cliente
+from app.models.plato import Plato
 from app.models.repartidor import Repartidor
 from app.models.token_revocado import TokenRevocado
 
 
-__all__ = ["Administrador", "Cliente", "Repartidor", "TokenRevocado"]
+__all__ = [
+    "Administrador",
+    "Cliente",
+    "Plato",
+    "Repartidor",
+    "TokenRevocado",
+]
